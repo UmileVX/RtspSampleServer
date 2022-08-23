4df783775c964244416b59dfc6388e4561d29113
YeonwooSung <neos960518@gmail.com>
2022-08-23 17:55:14 +0900

    Add log for debugging

--- src/initializeLiveStream.js
+++ src/initializeLiveStream.js
@@ -12,6 +12,9 @@ async function initializeIndividualStream(streamName) {
     var segment_file_path = playlist_file_path.replace('stream.m3u8', '%d.ts');
     // transcodeStream(streamName, rtsp_url);
 
+    console.log('playlist_file_path: ', playlist_file_path);
+    console.log('segment_file_path: ', segment_file_path);
+
     // generate child process for ffmpeg execution
     exec(`./run_ffmpeg.sh ${streamName} ${rtsp_url} ${playlist_file_path} ${segment_file_path}`, (err, stdout, stderr) => {
         if (err) {
