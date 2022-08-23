c85b1bb330284141f8b0229e5a4468789c39606c
YeonwooSung <neos960518@gmail.com>
2022-08-23 17:52:18 +0900

    Fix typos

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -20,9 +20,12 @@ do
         curl --head --connect-timeout 1 -i -X OPTIONS $rtsp_url
     done
 
+    echo "Rtsp stream is alive!"
+    curl http://localhost:3000/recover?streamName=$streamName
+
     # run ffmpeg
     echo "FFMPEG to transport rtsp :: ${rtsp_url}"
-    #TODO execute ffmpeg
+    # execute ffmpeg
     ffmpeg -fflags nobuffer -rtsp_transport tcp -i $rtsp_url -vsync 0 \
     -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments+append_list \
     -f segment -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts \
@@ -32,6 +35,7 @@ do
     echo "FFMPEG terminated"
     echo "Let the server know that the ffmpeg has been terminated!"
     curl http://localhost:3000/crash?streamName=$streamName
+    echo ""
 
     echo "Sleep for 5 seconds"
     sleep 5
