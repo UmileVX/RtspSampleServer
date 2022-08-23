1faeb1d17cd4f7b9e9dfff005993cb6062c376f9
YeonwooSung <neos960518@gmail.com>
2022-08-23 12:41:44 +0900

    Update Stream name

--- src/streamSrc.js
+++ src/streamSrc.js
@@ -1,9 +1,9 @@
 const KOGAS_STREAM = {
     // "stream name": "m3u8 directory path"
-    kogas_stream1: "/playlist/kogas_stream1/480p.m3u8",
-    kogas_stream2: "/playlist/kogas_stream2/480p.m3u8",
-    kogas_stream3: "/playlist/kogas_stream3/480p.m3u8",
-    kogas_stream4: "/playlist/kogas_stream4/480p.m3u8",
+    kogas_stream1: "/playlist/kogas_stream1/stream.m3u8",
+    kogas_stream2: "/playlist/kogas_stream2/stream.m3u8",
+    kogas_stream3: "/playlist/kogas_stream3/stream.m3u8",
+    kogas_stream4: "/playlist/kogas_stream4/stream.m3u8",
 }
 
 module.exports = {KOGAS_STREAM};
