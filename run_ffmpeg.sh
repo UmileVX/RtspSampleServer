0f15f30ddf33edd284521afda4b007567daa7e27
YeonwooSung <neos960518@gmail.com>
2022-08-26 17:49:03 +0900

    Make ffmpeg to output 6 hls outputs

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -26,9 +26,24 @@ do
     # run ffmpeg
     echo "FFMPEG to transport rtsp :: ${rtsp_url}"
     # execute ffmpeg
-    ffmpeg -fflags nobuffer -rtsp_transport tcp -i $rtsp_url -vsync 0 \
-    -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
-    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 $playlist_file_path
+    ffmpeg -fflags nobuffer -rtsp_transport tcp -i $rtsp_url \
+    -vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 ./public/playlist/kogas_stream1 \
+    -vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 ./public/playlist/kogas_stream2 \
+    -vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 ./public/playlist/kogas_stream3 \
+    -vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 ./public/playlist/kogas_stream4 \
+    -vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 ./public/playlist/kogas_stream5 \
+    -vsync 0 -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 ./public/playlist/kogas_stream6
+
+    # ffmpeg -fflags nobuffer -rtsp_transport tcp -i $rtsp_url -vsync 0 \
+    # -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments \
+    # -f hls -hls_delete_threshold 3 -hls_list_size 5 -hls_time 10 $playlist_file_path
+
     # ffmpeg -fflags nobuffer -rtsp_transport tcp -i $rtsp_url -vsync 0 \
     # -copyts -vcodec copy -movflags frag_keyframe+empty_moov -an -hls_flags delete_segments+append_list \
     # -f segment -segment_list_flags live -segment_time 10 -segment_list_size 3 -segment_format mpegts \
@@ -41,7 +56,13 @@ do
     echo ""
 
     # remove all hls related files for safety
-    rm -rf ./public/playlist/$streamName/*
+    # rm -rf ./public/playlist/$streamName/*
+    rm -rf ./public/playlist/kogas_stream1
+    rm -rf ./public/playlist/kogas_stream2
+    rm -rf ./public/playlist/kogas_stream3
+    rm -rf ./public/playlist/kogas_stream4
+    rm -rf ./public/playlist/kogas_stream5
+    rm -rf ./public/playlist/kogas_stream6
 
     echo "Sleep for 5 seconds"
     sleep 5
